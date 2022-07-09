bc8530cd9852caf2c01390d72b7d38b77023b862
Kenk <kenghin_lim@hotmail.com>
2022-07-09 18:23:32 +0800

    chore: Add Compile Version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -103,7 +103,15 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.10",
+        version: "0.8.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+          },
+        },
+      },
+      {
+        version: "0.8.4",
         settings: {
           optimizer: {
             enabled: true,
